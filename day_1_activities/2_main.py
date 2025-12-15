af57a308478013dd61d22d5637766cf106daa30a
ChloD2-0 <cdougherty1121@icloud.com>
2025-12-15 20:47:58 +0000

    Dictionaries1

--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -43,10 +43,25 @@ import student_data
 
 # print(student_data.students)
 students = student_data.students
-print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(len(students))
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+# print(students[0]['FName'])
+# print(students[0]['LName'])
+# print(students[0]['GL'])
+# #-------------------------
+# # get the second student's cpsid and first email and grade level
+# print(students[1]['CPSID'])
+# print(students[1]['Email'][0])
+# print(students[1]['GL'])
+# #------------------------
+# # get the last student's cpsid and last email and grade level and homeroom
+# print(students[-1]['CPSID'])
+# print(students[-1]['Email'][0])
+# print(students[-1]['GL'])
+# print(students[-1]['HR'])
+
 
 # What is being counted?
 
@@ -68,11 +83,36 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+
+# for student in students:
+        
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     # get the homeroom, grade level, and cpsid
+#     print(student['HR'])
+#     print(student['CPSID'])    
+#     if student['GL'] >= 10:
+#         print("You can go off campus")
+# # if the last name begins wiht the letter "R" give them a treat
+# # we print out their name and homeroom
+#     print("_"*25)
+#     if student['LName'][0] == "R":
+#         print(student['Combo,Name'])
+#         print(student['HR'])
+#     if student['LName'][-1] == "d" or student['LName'][-1] == "e":
+#         print(student['Combo,Name'])
+#         print(student['HR'])
+# # if the last name starts with the letter "L",
+# # break out of the loop
+#     if student['LName'][0] == "L":
+#         break
+
+
+#     print("_"*25)
+
+
+
 
 # What does the loop variable student represent on each pass?
 
